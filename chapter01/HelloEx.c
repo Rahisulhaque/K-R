4cb1dbef1a718259e5c4698d81a91ba7e116441f
Rahisul Haque <rahisulhaque@hotmail.com>
2017-11-05 12:04:17 -0800

    Expermenting \c

--- chapter01/HelloEx.c
+++ chapter01/HelloEx.c
@@ -5,7 +5,7 @@ int     main()
 {
     printf("Hello World!\n");
     printf("Hello \t again.");
-    printf("Hello \c from the otherside!);
+    printf("Hello \c from the otherside!");
 
     return (0);
 }
